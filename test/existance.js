a878532ed2fd38df9776e3970f468b381d433689
Yuval Dekel <thedekel@gmail.com>
2013-01-21 11:24:10 -0500

    cononected to db through mongolian

--- test/existance.js
+++ test/existance.js
@@ -1,12 +1,14 @@
 var assert = require('assert'),
-    remongo = require('./../index');
+    Remongo = require('./../index');
 
 suite('remongo', function(){
   test('remongo library file exists', function(){
-    assert.ok(remongo);
+    assert.ok(Remongo);
   });
   test('remongo properties and functions are defined', function(){
+    var remongo = new Remongo("test_db", "localhost", 27017);
     assert.ok(remongo.db);
-    assert.ok(remongo.createObject);
+    assert.ok(remongo.test_colls);
+    assert.ok(remongo.test_colls());
   });
 });

28cfab7f66699331667b56ae03f9728efe0119fe
Yuval Dekel <thedekel@gmail.com>
2013-01-27 22:38:40 -0500

    added some proof checking and instantiation cade

--- test/models.js
+++ test/models.js
@@ -5,6 +5,8 @@ suite('remongo', function(){
   test('new models can be created', function(){
     var remongo = new Remongo("test_db");
     var userScheme = remongo.createScheme();
+    var eventScheme = remongo.createScheme();
+    eventScheme.save("Event");
     userScheme.publics({
       name: String
     });
@@ -12,18 +14,32 @@ suite('remongo', function(){
       password: String
     });
     userScheme.derives({
-      events: {
-        model: "Event",
-        array: true
-      }
+      events: ["Event"]
     });
     userScheme.save("User");
     assert.ok(remongo.models.User);
-    var testInstance = new remongo.models.User({name: "test", password:"123456"});
+    var testInstance = new remongo.models.User({name: "test", password:"123456", events:[new remongo.models.Event({})]});
     assert.ok(testInstance.save);
     assert.ok(testInstance.remove);
     assert.ok(testInstance.values);
     console.log(testInstance.values);
+  });
+
+  test("models can't be created with missing fields", function(){
+    var remongo = new Remongo("test_db");
+    var userScheme = remongo.createScheme()
+      .publics({
+        name: String,
+        email: "EmailObject"
+      })
+      .privates({
+        password: String
+      });
+    userScheme.save("User");
+  userScheme.save("User");
+  assert.throws(function(){
+    var testInstance = new remongo.models.User({name: "test", password:"123456"});
+  });
   });
 });
  

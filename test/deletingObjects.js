e18fdb31969cf2b31aa13e923d21ad7b638720f9
Yuval Dekel <thedekel@gmail.com>
2013-08-11 11:27:30 -0400

    propogated remove and a little async magic :shipit:

--- test/deletingObjects.js
+++ test/deletingObjects.js
@@ -3,6 +3,8 @@ var Remongo = require('./../index');
 //init simple schema
 var remongo = new Remongo('deletions_db');
 var simpleUserS = remongo.createSchema();
+var simpleUserS2 = remongo.createSchema();
+var postS = remongo.createSchema();
 
 simpleUserS.publics({
   name: String,
@@ -11,8 +13,23 @@ simpleUserS.publics({
 simpleUserS.privates({
   pass: String
 });
+simpleUserS2.publics({
+  name: String,
+  email: String
+});
+simpleUserS2.privates({
+  pass: String
+});
+postS.publics({
+  content: String
+});
+postS.embeds({
+  author: "User2"
+});
 
 simpleUserS.save("User");
+simpleUserS2.save("User2");
+postS.save("Post");
 
 exports['clear collection of simple instances'] = function(be, assert) {
   var simpleInstance = new remongo.models.User(
@@ -31,4 +48,31 @@ exports['clear collection of simple instances'] = function(be, assert) {
   });
 };
 
+exports['embedded instances of an object are removed'] = function(be, assert) {
+  var simpleInstance = new remongo.models.User2(
+    {
+      name: "testUser7",
+      email: "test@user7.com",
+      pass: "password"
+    }
+  );
+  simpleInstance.save(function(err, doc) {
+    if (err) assert.fail(err);
+    var simplePost = new remongo.models.Post(
+      {
+        content: "test hammers"
+      }
+    );
+    simplePost.embed("author", doc);
+    simplePost.save(function(err, postDoc /*lol, postDoc*/) {
+      remongo.models.User2.findById(doc.values._id, function(err, instance){
+        if (err) assert.fail(err);
+        if (!instance) assert.fail('no instance in database');
+        instance.remove(function(err) {
+          if (err) assert.fail(err);
+        });
+      });
+    });
+  });
+};
 
